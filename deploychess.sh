23222e75fe2d49af2c49130cd7de0839570415ee
qqap <lavahumour@gmail.com>
2024-08-08 17:20:04 +0530

    patch

--- deploychess.sh
+++ deploychess.sh
@@ -1,5 +1,5 @@
 dsi() { docker stop $(docker ps -a | awk -v i="^$1.*" '{if($2~i){print$1}}'); }
 dsi chess
-docker rmi chess # remove the old image
+docker rmi -f $(docker images -q chess) # remove the old image
 docker load -i /home/a/chess.tar # this causes rename
 docker run -d -p 3000:9000 chess mono WebApp.exe
